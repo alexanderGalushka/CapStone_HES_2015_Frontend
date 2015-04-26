ff7113296f7ffecd64f082f09acd47d27786db76
ibugarinovic <ibugarinovic@yahoo.com>
2015-04-26 14:29:07 -0400

    Plate editor

--- app/plateeditor/singlewell/singlewell.directive.js
+++ app/plateeditor/singlewell/singlewell.directive.js
@@ -14,6 +14,7 @@
           dropgroup:"=",
           "labels":"=",
           "rows":"=",
+          "controltypes":"=",
           "plate":"="
         },
         templateUrl: 'plateeditor/singlewell/singlewell.html',
